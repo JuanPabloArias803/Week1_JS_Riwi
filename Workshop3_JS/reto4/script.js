aa4c3fca53aa2fe72fe5d4f0d7d959bf3cf3e778
JuanPabloArias803 <juanpabloarias803@gmail.com>
2024-04-23 21:26:26 -0500

    subir cambios

--- Workshop3_JS/reto4/script.js
+++ Workshop3_JS/reto4/script.js
@@ -18,11 +18,12 @@ const createEvent=function(){
     let date=prompt("Cuando es la fecha del evento");
     let description=prompt("Añadir descripcion adicional del evento");
     events.push({id,name,date,description});
+    alert("Evento creado satisfactoriamente");
 }
 
-const showEvents=function(){
+const showEvents=function(array){
     let message="";
-    events.forEach(element => {
+    array.forEach(element => {
         message+=element.id+". "+element.name+"\n  Fecha: "+element.date+"\n  Descripción: "+element.description+"\n";
     });
     return message;
@@ -38,15 +39,72 @@ while(i){
                 id++;
                 break
             case 2:
-                alert(showEvents());
+                alert(showEvents(events));
                 break
             case 3:
                 break
             case 4:
-                choice=Number(prompt("¿Que id de evento quieres modificar?\n"+showEvents()));
-                break
+                let j=true;
+                let eventSelect={};
+                while(j){
+                    let flag=false;
+                    choice=Number(prompt("¿Que id de evento quieres modificar?\n"+showEvents(events)));
+                    events.forEach(element => {
+                        if(element.id==choice){
+                            flag=true;
+                            let k=true;
+                            eventSelect=element;
+                            while(k){
+                                choice=Number(prompt("¿Que deseas modificar\n1. Nombre\n2. Fecha\n3. Descripción\n4. Cancelar"));
+                                if((choice>0)&&(choice<5)){
+                                    switch(choice){
+                                        case 1:
+                                            eventSelect.name=prompt("Introduzca el nuevo nombre del evento");
+                                            for (let i = 0; i < events.length; i++) {
+                                                if(events[i].id==eventSelect.id){
+                                                    events[i]=eventSelect;
+                                                }
+                                            }
+                                            alert("Cambios guardados satisfactoriamente");
+                                            break;
+                                        case 2:
+                                            eventSelect.date=prompt("Introduzca la nueva fecha del evento");
+                                            for (let i = 0; i < events.length; i++) {
+                                                if(events[i].id==eventSelect.id){
+                                                    events[i]=eventSelect;
+                                                }
+                                            }
+                                            alert("Cambios guardados satisfactoriamente");
+                                            break;
+                                        case 3:
+                                            eventSelect.description=prompt("Introduzca la nueva descripción del evento");
+                                            for (let i = 0; i < events.length; i++) {
+                                                if(events[i].id==eventSelect.id){
+                                                    events[i]=eventSelect;
+                                                }
+                                            }
+                                            alert("Cambios guardados satisfactoriamente");
+                                            break;
+                                        case 4:
+                                            k=false;
+                                            break;
+                                    }
+                                }else{
+                                    alert("Introduzca un valor válido");
+                                }
+                            }    
+                        }
+                    });
+                    if(flag==true){
+                        j=false;
+                        break;
+                    }else{
+                    alert("Introduce un evento de la lista")
+                    }
+                }
+                break;
             case 5:
-                choice=Number(prompt("¿Que id de evento quieres eliminar?\n"+showEvents()));
+                choice=Number(prompt("¿Que id de evento quieres eliminar?\n"+showEvents(events)));
                 break
             case 6:
                 i=false;
